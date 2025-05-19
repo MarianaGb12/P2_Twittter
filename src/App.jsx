249583e3a39a334c18b2b94082ff79134bc852ef
MarianaGb12 <mariana.guerrero021516@gmail.com>
2025-05-19 10:03:49 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -8,57 +8,60 @@ import Profile from "./components/private/Profile";
 import CreatePost from "./components/private/CreatePost"; //
 import "./App.css";
 import { GrowthBook, GrowthBookProvider } from "@growthbook/growthbook-react";
+import { AuthProvider } from "./context/AuthContext";
 
 // Crea una instancia de GrowthBook
 const growthbook = new GrowthBook({
   apiHost: "https://cdn.growthbook.io",
-  clientKey: process.env.REACT_APP_GROWTHBOOK_CLIENT_KEY, // Tu API Key de GrowthBook
+  clientKey: import.meta.env.VITE_GROWTHBOOK_CLIENT_KEY,
   enableDevMode: true, // Habilita el modo desarrollo para pruebas
   features: {
     showButton: {
-      defaultValue: false, // Valor por defecto del flag
+      defaultValue: true, // cambiar a true para ver los botones
     },
   },
 });
 
 function App() {
   return (
-    <GrowthBookProvider growthbook={growthbook}>
-      <Router>
-        <Routes>
-          {/* Public Routes */}
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+    <AuthProvider>
+      <GrowthBookProvider growthbook={growthbook}>
+        <Router>
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
-          {/* Private Routes */}
-          <Route
-            path="/home"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/profile"
-            element={
-              <PrivateRoute>
-                <Profile />
-              </PrivateRoute>
-            }
-          />
-          <Route
-            path="/create-post"
-            element={
-              <PrivateRoute>
-                <CreatePost />
-              </PrivateRoute>
-            }
-          />
-        </Routes>
-      </Router>
-    </GrowthBookProvider>
+            {/* Private Routes */}
+            <Route
+              path="/home"
+              element={
+                <PrivateRoute>
+                  <Home />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/profile"
+              element={
+                <PrivateRoute>
+                  <Profile />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/create-post"
+              element={
+                <PrivateRoute>
+                  <CreatePost />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </GrowthBookProvider>
+    </AuthProvider>
   );
 }
 
